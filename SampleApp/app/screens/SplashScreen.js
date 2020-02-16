d6076d52ef82c5708bb1593ffde520e069666346
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-02-16 23:44:32 +0500

    Added splash screen on app start.

---
+++ SampleApp/app/screens/SplashScreen.js
@@ -0,0 +1,97 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Button,
+    Alert,
+    ActivityIndicator,
+    Image,
+    Animated
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { TextInput } from 'react-native-gesture-handler';
+
+class ImageLoader extends React.Component {
+    state = {
+        opacity: new Animated.Value(0),
+    }
+
+    onLoad = () => {
+        Animated.timing(this.state.opacity, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: true
+        }).start();
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return (
+            <>
+                <Animated.Image
+                    onLoad={this.onLoad}
+                    {...this.props}
+                    // onLoadEnd={ () => { this.props.navigation.navigate('Home') } }
+                    style={[
+                        {
+                            opacity: this.state.opacity,
+                            transform: [
+                                {
+                                    scale: this.state.opacity.interpolate({
+                                        inputRange: [0, 1],
+                                        outputRange: [0.70, 1]
+                                    })
+                                }
+                            ]
+                        },
+                        this.props.style
+                    ]}
+                />
+            </>
+        )
+    }
+}
+
+export default class SplashScreen extends React.Component {
+    render() {
+        return (
+            <>
+                <View style={styles.container}>
+                    <ImageLoader source={require('../../assets/foodvise-logo.png')} />
+                </View>
+
+            </>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#db3545',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
